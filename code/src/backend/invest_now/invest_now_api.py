1710a8c6c8186ac8d89fb0d2642c2b8dd368c5cd
21f3001967 <85440504+The-coderlearner@users.noreply.github.com>
2025-03-25 12:33:11 +0530

    chore: initial commit for invest now

---
+++ code/src/backend/invest_now/invest_now_api.py
@@ -0,0 +1,22 @@
+from invest_now_model import *
+import pandas as pd
+
+# we integrate api for invest now
+
+def invest_now(cus_id = "CUST2025B"):
+    risk_level = input("Risk level: ")
+    max_tenure = int(input("Tenure: "))
+    if(max_tenure >7):
+        ten_str = "long"
+    elif (max_tenure<=7 or max_tenure>3):
+        ten_str = "medium"
+    else:
+        ten_str = "short"
+    file_path = '../datasets/customer_dataset.xlsx'
+    DATA = classify_investment_insight(pd.read_excel(file_path), cus_id)
+    text = str(risk_level) + " risk and " + ten_str + " term" + " and " + DATA[0]
+    print(text)
+    op = final_investment_list(text)
+    print(op)
+    return op
+invest_now()
